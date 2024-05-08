114d40c8d8f80669338579cdba2c68b5c1479ad3
dsdfgrdg <psjcedronitro13@hotmail.com>
2024-05-08 17:56:00 +0700

    update project

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
-function rob(nums) {
-  if (nums.length === 1) return nums[0];
-  const robRange = (start, end) => {
-    let prevMax = 0;
-    let currMax = 0;
-    for (let i = start; i <= end; i++) {
-      const temp = currMax;
-      currMax = Math.max(currMax, prevMax + nums[i]);
-      prevMax = temp;
+function largestDivisibleSubset(nums) {
+  nums.sort((a, b) => a - b);
+  const dp = new Array(nums.length).fill(1);
+  let maxSubsetSize = 1;
+  let maxSubsetIdx = 0;
+  for (let i = 1; i < nums.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (nums[i] % nums[j] === 0) {
+        dp[i] = Math.max(dp[i], dp[j] + 1);
+        if (dp[i] > maxSubsetSize) {
+          maxSubsetSize = dp[i];
+          maxSubsetIdx = i;
+        }
+      }
     }
-    return currMax;
-  };
-  return Math.max(robRange(0, nums.length - 2), robRange(1, nums.length - 1));
+  }
+  const result = [];
+  let prev = nums[maxSubsetIdx];
+  let count = maxSubsetSize;
+  for (let i = maxSubsetIdx; i >= 0; i--) {
+    if (prev % nums[i] === 0 && dp[i] === count) {
+      result.unshift(nums[i]);
+      prev = nums[i];
+      count--;
+    }
+  }
+  return result;
 }
